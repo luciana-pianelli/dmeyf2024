c59fa8e99e862a6aaea3f237a080768228c99d6b
luciana-pianelli <lucianapianelli@gmail.com>
2024-11-19 12:04:44 +0000

    auto commit 2       20241119 120439

--- src/workflows/lp916_workflow_exp4_f202106.r
+++ src/workflows/lp916_workflow_exp4_f202106.r
@@ -388,7 +388,7 @@ SC_scoring <- function( pinputexps )
 {
   if( -1 == (param_local <- exp_init())$resultado ) return( 0 )# linea fija
 
-  param_local$meta$script <- "/src/wf-etapas/z2401_SC_scoring_lightgbm.r"
+  param_local$meta$script <- "/src/wf-etapas/2401_SC_scoring_lightgbm_exp.r"
 
   param_local$semilla <- NULL  # no usa semilla, es deterministico
 
