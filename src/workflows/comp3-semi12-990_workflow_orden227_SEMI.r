5cc6396538da8d534bfee4b81f09337a4f60116a
luciana-pianelli <lucianapianelli@gmail.com>
2024-11-30 16:53:43 -0300

    LA12

--- src/workflows/comp3-semi12-990_workflow_orden227_SEMI.r
+++ src/workflows/comp3-semi12-990_workflow_orden227_SEMI.r
@@ -483,7 +483,7 @@ KA_evaluate_kaggle_semillerio <- function( pinputexps )
 # Que predice 202107 donde conozco la clase
 # y ya genera graficos
 
-wf_c3SEMI8_sep_orden227 <- function( pnombrewf )
+wf_c3SEMI12_sep_orden227 <- function( pnombrewf )
 {
   param_local <- exp_wf_init( pnombrewf ) # linea fija
 
@@ -524,6 +524,6 @@ wf_c3SEMI8_sep_orden227 <- function( pnombrewf )
 # Aqui comienza el programa
 
 # llamo al workflow con future = 202108
-wf_c3SEMI8_sep_orden227()
+wf_c3SEMI12_sep_orden227()
 
 
